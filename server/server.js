5721bdc6c1d6a4dca244e3ed679eef9d40c9acb8
Mohammad-Rehan-Ahmed <rehanahmed631743@gmail.com>
2026-07-07 10:54:06 +0530

    Remove environment variable logging

--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
 const dotenv = require("dotenv");
 dotenv.config();
-console.log("PORT =", process.env.PORT);
-console.log("MONGO_URI =", process.env.MONGO_URI);
-console.log("GROQ_API_KEY =", process.env.GROQ_API_KEY);
 
 const connectDB = require("./config/db");
 const app = require("./app");
@@ -13,7 +10,7 @@ const startServer = async () => {
     await connectDB();
 
     app.listen(PORT, () => {
-        console.log(`🚀 Server is running on http://localhost:${PORT}`);
+        console.log(`🚀 Server is running on port ${PORT}`);
     });
 };
 
